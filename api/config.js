cada1935b8add32b8ecca61f9a50b765dacca01a
firstday-alex <alex.turney@firstday.com>
2026-04-04 00:24:16 -0500

    more

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.setHeader("Cache-Control", "no-store");
   res.json({
-    shopifyDomain: process.env.SHOPIFY_DOMAIN || "",
+    shopifyDomain: process.env.SHOPIFY_URL || "",
     shopifyToken: process.env.SHOPIFY_TOKEN || "",
     sheetUrl: process.env.SHEET_URL || "",
   });
